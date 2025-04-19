28c04b1bd66a99257804d225dabeb59237882db7
Yumna Mubeen <yumnamubeen0019@gamil.com>
2025-04-19 16:09:04 +0500

    link

--- backend/index.js
+++ backend/index.js
@@ -6,7 +6,7 @@ require('dotenv').config();
 const app = express();
 // app.use(cors());
 app.use(cors({
-  origin: "https://mongo-auth-app.vercel.app", // your frontend URL
+  origin: "*", // your frontend URL   : https://mongo-auth-app.vercel.app
   methods: ["GET", "POST", "PUT", "DELETE"],
 }));
 app.use(express.json());
